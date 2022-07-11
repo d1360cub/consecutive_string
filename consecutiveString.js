6d54929de71e85b799331038e107efa2a56accbf
ojosdegatonegro <diegofernandocubides@gmail.com>
2022-07-11 17:49:05 -0500

    Adds code to find longest consecutive string

--- consecutiveString.js
+++ consecutiveString.js
@@ -1,9 +1,27 @@
 function longestConsec(strarr, k) {
+  let current = '';
+  let maxLength = 0;
+  let result = '';
   if (strarr.length === 0 || k > strarr.length || k <= 0) {
     return '';
   }
-  return 'initialCommit';
+
+  for (let i = 0; i < strarr.length - 1; i++) {
+    current = strarr[i];
+    for (let j = 0; j < k - 1; j++) {
+      current += strarr[i + j + 1];
+      if (current.length > maxLength) {
+        maxLength = current.length;
+        result = current;
+      }
+    }
+  }
+  return result;
 }
+
+console.log(
+  longestConsec(['tree', 'foling', 'trashy', 'blue', 'abcdef', 'uvwxyz'], 2)
+);
 
 console.log(
   longestConsec(
